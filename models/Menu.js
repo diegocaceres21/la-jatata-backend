ac65e6c801741a4851c71468a8519beaf277a61d
diegocaceres21 <96498718+diegocaceres21@users.noreply.github.com>
2022-10-25 17:14:22 -0400

    Comanda patch and token

--- models/Menu.js
+++ models/Menu.js
@@ -12,7 +12,8 @@ const MenuSchema = mongoose.Schema({
         product_id: Number,
         product_name: String,
         prepared:Number,
-        reservated:Number
+        //reservated:Number,
+        //delivered:Number
     }],
 },{ _id: false });
 
