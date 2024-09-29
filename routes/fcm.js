f23ee3103bd2a59ed029536a988aaaf67e9edb64
Diego <diego.caceres@ucb.edu.bo>
2024-09-28 20:09:40 -0400

    Registro Topic

--- routes/fcm.js
+++ routes/fcm.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
-const serviceAccount =  JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
 const router = express.Router();
 
 admin.initializeApp({
@@ -16,19 +16,41 @@ const sendNotification = async (token, payload) => {
         console.error('Error sending message:', error);
     }
 };
+
+router.post('/subscribe-to-topic', async (req, res) => {
+    const { token, topic } = req.body; // Expect token and topic in request body
+
+    try {
+        // Subscribe the token to the topic
+        const response = await admin.messaging().subscribeToTopic(token, topic);
+        console.log('Successfully subscribed to topic:', response);
+        res.status(200).send({ success: true, response });
+    } catch (error) {
+        console.error('Error subscribing to topic:', error);
+        res.status(500).send({ success: false, error });
+    }
+});
 router.post('/send-notification', (req, res) => {
-    const { topic, message, data } = req.body; // Extracting data from request body
+    const { topic, message } = req.body; // Extracting data from request body
 
     const payload = {
+        topic: topic, // Replace with your topic name
+        notification: {
+            title: 'Nueva Comanda',
+            body: 'Revisar pedido para la Jatata',
+        },
+    };
+
+    /*const payload = {
         notification: {
             title: 'Nuevo Pedido',
             body: message,
         },
         data: data, // Include data payload here
-    };
+    };*/
 
     // Send a message to the devices subscribed to the provided topic
-    admin.messaging().sendToTopic(topic, payload)
+    admin.messaging().send(payload)
         .then((response) => {
             console.log('Notification sent successfully:', response);
             res.status(200).send('Notification sent successfully');
